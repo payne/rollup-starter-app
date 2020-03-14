4abe9d7e24004011b36beeb7564ba65166b551c9
Matt Payne <Payne@MattPayne.org>
2020-03-14 07:32:24 -0500

    dry it up by eleminating code duplication.

--- src/main.js
+++ src/main.js
@@ -6,12 +6,11 @@ import TextClock from './matt1';
 // logs will still point to your original source modules
 console.log('if you have sourcemaps enabled in your devtools, click on main.js:5 -->');
 
-update();
-const span = document.querySelector('#time-now-matt1');
-const tc = new TextClock(span,60*10*1000);
-tc.update2();
+[1,2].forEach(spanNumber => {
+    const span = document.querySelector(`#time-now-matt${spanNumber}`);
+    const tc = new TextClock(span,60*spanNumber*10*1000);
+    tc.update2();
+});
 
-const span2 = document.querySelector('#time-now-matt2');
-const tc2 = new TextClock(span2,60*20*1000);
-tc2.update2();
+update();
 
