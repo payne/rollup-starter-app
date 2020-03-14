21a519578834f2195a7bd64d16a72258cceccff5
Matt Payne <Payne@MattPayne.org>
2020-03-13 20:40:52 -0500

    Two instances ten minutes apart. :-)

--- src/main.js
+++ src/main.js
@@ -8,7 +8,10 @@ console.log('if you have sourcemaps enabled in your devtools, click on main.js:5
 
 update();
 const span = document.querySelector('#time-now-matt1');
-const tc = new TextClock(span);
+const tc = new TextClock(span,60*10*1000);
 tc.update2();
 
+const span2 = document.querySelector('#time-now-matt2');
+const tc2 = new TextClock(span2,60*20*1000);
+tc2.update2();
 
--- src/matt1.js
+++ src/matt1.js
@@ -2,24 +2,27 @@ import format from 'date-fns/format';
 
 
 export default class TextClock {
-    constructor(span) {
+    constructor(span, delta) {
         this.span=span;
+        this.delta = delta;
     }
 
     update2() {
        this.doWork(this.tick);
     }
 
-    tick(sp) {
+    tick(sp, delta) {
         console.log(sp);
-        sp.textContent = format(new Date(), 'h:mm:ssa');
+        const date = new Date();
+        const d = new Date(date.getTime() + delta);
+        sp.textContent = format(d, 'h:mm:ssa');
     }
 
     // from
     // https://odetocode.com/blogs/scott/archive/2015/01/05/more-with-arrow-functions-in-es6.aspx
     doWork(callback) {
         setTimeout(() => {
-            callback(this.span)
+            callback(this.span, this.delta);
             this.doWork(callback);
         }, 1000);
     }
